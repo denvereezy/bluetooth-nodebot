d2e82dab912f08c956a18e6deb4d84a389f71e49
denver daniels <denver@projectcodex.co>
2016-04-16 09:14:42 +0200

    wheels rolling

--- wheels.js
+++ wheels.js
@@ -1,47 +1,38 @@
 const five = require("johnny-five");
 
-const Wheel = function(pwmPin, directPin, speedParam) {
-    const motor = new five.Motor({
-            pins: {
-                pwm: pwmPin,
-                dir: directPin
-            },
-            invertPWM: true
-        }),
-        speed = speedParam || 100;
+const RightWheel = function(pwmPin, directPin, speedParam){
+  const motor = new five.Motor({
+      pins: {
+        pwm: pwmPin,
+        dir: directPin
+      },
+      invertPWM: true
+  }),
+  speed = speedParam || 256;
 
 
-        const motor2 = new five.Motor({
-                pins: {
-                    pwm: pwmPin,
-                    dir: directPin
-                },
-                invertPWM: true
-            });
-          //  speed = speedParam || 256;
+  this.stop = function(){
+    motor.stop();
+  }
 
-    this.stop = function() {
-        motor.stop();
-    }
+  this.forward = function(){
+    motor.forward(speed);
+  }
 
-    this.forward = function() {
-        motor2.forward(speed);
-    }
+  this.reverse = function(){
+    motor.reverse(speed);
+  }
 
-    this.reverse = function() {
-        motor2.reverse(speed);
-    }
+  this.faster = function (increase) {
+    increase = increase || 1;
+    speed += increase;
+  }
 
-    this.faster = function(increase) {
-        increase = increase || 1;
-        speed += increase;
-    }
-
-    this.slower = function(decrease) {
-        decrease = decrease || 1;
-        speed -= decrease;
-    }
+  this.slower = function (decrease) {
+    decrease = decrease || 1;
+    speed -= decrease;
+  }
 
 }
 
-module.exports = Wheel;
+module.exports = RightWheel;

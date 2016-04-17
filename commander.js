db4759162a7f505a1b49e1e09be84cc742a9f4d2
denver daniels <denver@projectcodex.co>
2016-04-17 08:46:38 +0200

    speech module added

--- commander.js
+++ commander.js
@@ -3,7 +3,8 @@ const five       = require("johnny-five"),
       FrontWheel = require("./frontWheel"),
       RearWheel  = require('./rearWheel'),
       Robot      = require("./robot"),
-      RobotRun   = require('./robotRun');
+      RobotRun   = require('./robotRun'),
+      say        = require('say');
 
 keypress(process.stdin);
 
@@ -12,9 +13,9 @@ const board = new five.Board({
 });
 
 board.on("ready", function() {
-
+say.speak('You are ready to roll', 'Alex',1);
     const frontWheel = new FrontWheel(4, 5, 180),
-        rearWheel = new RearWheel(6, 7, 200);
+        rearWheel = new RearWheel(6, 7, 256);
 
     const robot = new Robot(frontWheel);
     this.repl.inject({
@@ -34,26 +35,32 @@ board.on("ready", function() {
             switch (key.name) {
                 case 'q':
                     console.log('Quitting');
+                    say.speak('good bye robots', 'Alex', 1);
                     process.exit();
                     break;
                 case 'up':
                     console.log('Forward');
+                    say.speak('moving forward', 'Alex', 1);
                     robotRun.forward();
                     break;
                 case 'down':
                     console.log('Backwards');
+                    say.speak('reversing peeps', 'Alex', 1);
                     robotRun.reverse();
                     break;
                 case 'left':
                     console.log('left');
+                    say.speak('to the left, to the left', 'Alex', 2);
                     robot.left();
                     break;
                 case 'right':
                     console.log('right');
+                    say.speak('right again', 'Alex', 1);
                     robot.right();
                     break;
                 case 'space':
                     console.log('stop');
+                    say.speak('holding the horses', 'Alex', 1);
                     robot.stop();
                     robotRun.stop();
                     break;
